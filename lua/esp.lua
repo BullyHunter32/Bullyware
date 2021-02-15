558dde83c729883f226621c9d24e7e1fb518c6a1
BullyHunter32 <60613196+BullyHunter32@users.noreply.github.com>
2021-02-15 14:02:44 +0000

    ESP Update
    Updated esp.lua

--- lua/esp.lua
+++ lua/esp.lua
@@ -1,3 +1,13 @@
+--[[
+    ____        ______     _       __              
+   / __ )__  __/ / / /_  _| |     / /___ _________ 
+  / __  / / / / / / / / / / | /| / / __ `/ ___/ _ \
+ / /_/ / /_/ / / / / /_/ /| |/ |/ / /_/ / /  /  __/
+/_____/\__,_/_/_/_/\__, / |__/|__/\__,_/_/   \___/ 
+                  /____/                           
+]]--
+
+
 local GetAllPlayers = player.GetAll
 local ipairs = ipairs
 local addHook = hook.Add
@@ -12,7 +22,8 @@ local SetDrawColor = surface.SetDrawColor
 local SetTextColor = surface.SetTextColor
 local GetTeamColor = team.GetColor
 local AddHalo = halo.Add
-
+local sqrt = math.sqrt
+local floor = math.floor
 local function drawBones( ply )
     local Bones = {}
     for k, v in ipairs(bones) do
@@ -196,11 +207,46 @@ methamphetamine.espentities = methamphetamine.GetESPEntites()
 --         surface.DrawText( text )
 --     end
 -- end)
+
+local cachedTeamColors = {}
+methamphetamine.GetTeamColor = function(iTeam)
+    if not cachedTeamColors[iTeam] then
+        cachedTeamColors[iTeam] = GetTeamColor(iTeam)
+    end
+    return cachedTeamColors[iTeam]
+end
+
+local cachedTeamNames = {}
+methamphetamine.GetTeamName = function(iTeam)
+    if not cachedTeamNames[iTeam] then
+        cachedTeamNames[iTeam] = team.GetName(iTeam)
+    end
+    return cachedTeamNames[iTeam]
+end
+
+local function calculateTextOffSet( strText, strFont, iAlignX, iAlignY ) -- 1 and 4 is pretty much all i use
+    iAlignX = iAlignX or 0
+    iAlignY = iAlignY or 0
+    surface.SetFont(strFont)
+    local textWide,textTall = surface.GetTextSize(strText)
+    local offsetx,offsety = 0,0
+    if iAlignX == 1 then
+        offsetx = offsetx - (textWide/2)
+    elseif iAlignX == 2 then
+        offsetx = offsetx - textWide
+    end
+    if iAlignY == 4 then
+        offsety = offsety + textTall
+    elseif iAlignY == 2 then
+        offsety = offsety - (textTall/2)
+    end
+    return offsetx,offsety
+end
+
 local colors 
 addHook("HUDPaint","methamphetamine.esp",function()
     if methamphetamine.screengrab then return end
     colors = methamphetamine.mods and methamphetamine.mods["ESP"] and methamphetamine.mods["ESP"].colors or {}
-
 
     if CurTime() - methamphetamine.lastscreengrab < 5 then
         surface.SetDrawColor( vguicolors.background )
@@ -217,10 +263,15 @@ addHook("HUDPaint","methamphetamine.esp",function()
     for k , v in ipairs( GetAllPlayers() ) do
         if v:Health() <= 0 or v == LocalPlayer() then goto skiprender end
         if LocalPlayer():GetPos():DistToSqr( v:GetPos() ) > methamphetamine.mods["ESP"].Range*methamphetamine.mods["ESP"].Range then goto skiprender end
-        local teamcolor = GetTeamColor( v:Team() )
+        local LEFT_MARGIN = 0
+        local TOP_MARGIN = 0
+        local RIGHT_MARGIN = 0
+        local BOTTOM_MARGIN = 0
+        local teamcolor = methamphetamine.GetTeamColor(v:Team())
         local healthcolor = Color( 255 * (1 - (v:Health() / v:GetMaxHealth())) , 255 * (v:Health()/v:GetMaxHealth()), 0 )
         local MaxX,MaxY,MinX,MinY,V1,V2,V3,V4,V5,V6,V7,V8 = getBounds( v )
-
+        local distance = floor(sqrt(LocalPlayer():GetPos():DistToSqr(v:GetPos())))
+        
         if methamphetamine.mods["ESP"].enabled["Box"] then
             if methamphetamine.mods["ESP"].colortype["Box"] == "Selected Color" then
                 SetDrawColor( colors.Box or color_white )
@@ -273,15 +324,15 @@ addHook("HUDPaint","methamphetamine.esp",function()
         if methamphetamine.mods["ESP"].enabled["Health [Bar]"] then
             --print("Drawing health bar")
             local width = 6
+            local margin = width*2
             local diff = (MaxY - MinY) - ((MaxY - MinY) * math.min( math.max(v:Health(),0) / v:GetMaxHealth(),1))
-            local poly = {
-                { x = MaxX + width, y = MaxY  },
-                { x = MaxX + (width*2) , y = MaxY },
-                { x = MaxX + (width*2) , y = MinY },
-                { x = MaxX + (width) , y = MinY }, 
-            }
             surface.SetDrawColor( color_black )
-            surface.DrawPoly(poly)
+            surface.DrawPoly({
+                { x = MaxX + margin, y = MaxY  },
+                { x = MaxX + width , y = MaxY },
+                { x = MaxX + width, y = MinY },
+                { x = MaxX + margin , y = MinY }, 
+            })
 
             if methamphetamine.mods["ESP"].colortype["Health [Bar]"] == "Selected Color" then
                 SetDrawColor( colors["Health [Bar]"] or color_white )
@@ -294,11 +345,235 @@ addHook("HUDPaint","methamphetamine.esp",function()
             end
 
             surface.DrawPoly({
-                { x = MaxX + width + 1, y = MaxY - 1  },
-                { x = MaxX + (width*2 - 1), y = MaxY - 1 },
-                { x = MaxX + (width*2 - 1), y = MinY + diff + 1 },
-                { x = MaxX + (width + 1), y = MinY + diff + 1 }, 
+                { x = MaxX + margin - 1, y = MaxY - 1},
+                { x = MaxX + width + 1, y = MaxY - 1},
+                { x = MaxX + width + 1, y = MinY + 1 + diff},
+                { x = MaxX + margin - 1, y = MinY + 1 + diff}, 
             })
+        end
+        local _,TEXT_MARGIN_INCREMENT = surface.GetTextSize("012376543")
+        if methamphetamine.mods["ESP"].enabled["Name"] then
+            if methamphetamine.mods["ESP"].colortype["Name"] == "Selected Color" then
+                SetTextColor( colors["Name"] or color_white )
+            elseif methamphetamine.mods["ESP"].colortype["Name"] == "Team Color" then
+                SetTextColor( teamcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Name"] == "Health Color" then
+                SetTextColor( healthcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Name"] == "Rainbow" then
+                SetTextColor( rainbow )
+            end
+            local nameRenderType = methamphetamine.mods["ESP"].types["Name"]
+            local str = v.SteamName and v:SteamName() or v:Nick()
+            if nameRenderType == "Top" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,4)
+                surface.SetTextPos(x + offsetx,MinY-TOP_MARGIN-offsety)
+                surface.DrawText(str)
+                TOP_MARGIN = TOP_MARGIN + TEXT_MARGIN_INCREMENT
+            elseif nameRenderType == "Left" then
+                local x = MinX - 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,2,1)
+                surface.SetTextPos(x + offsetx,MinY-LEFT_MARGIN+offsety)
+                surface.DrawText(str)
+                LEFT_MARGIN = LEFT_MARGIN - TEXT_MARGIN_INCREMENT
+            elseif nameRenderType == "Right" then
+                local x = MaxX + 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,0,1)
+                surface.SetTextPos(x + offsetx,MinY-RIGHT_MARGIN+offsety)
+                surface.DrawText(str)
+                RIGHT_MARGIN = RIGHT_MARGIN - TEXT_MARGIN_INCREMENT
+            elseif nameRenderType == "Bottom" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,0)
+                surface.SetTextPos(x + offsetx,MaxY+BOTTOM_MARGIN+offsety)
+                surface.DrawText(str)
+                BOTTOM_MARGIN = BOTTOM_MARGIN + TEXT_MARGIN_INCREMENT
+            end
+        end
+        if methamphetamine.mods["ESP"].enabled["Rank"] then
+            if methamphetamine.mods["ESP"].colortype["Rank"] == "Selected Color" then
+                SetDrawColor( colors["Rank"] or color_white )
+            elseif methamphetamine.mods["ESP"].colortype["Rank"] == "Team Color" then
+                SetDrawColor( teamcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Rank"] == "Health Color" then
+                SetDrawColor( healthcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Rank"] == "Rainbow" then
+                SetDrawColor( rainbow )
+            end
+            local rankRenderType = methamphetamine.mods["ESP"].types["Rank"]
+            local str = v:GetUserGroup()
+            if rankRenderType == "Top" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,4)
+                surface.SetTextPos(x + offsetx,MinY-TOP_MARGIN-offsety)
+                surface.DrawText(str)
+                TOP_MARGIN = TOP_MARGIN + TEXT_MARGIN_INCREMENT
+            elseif rankRenderType == "Left" then
+                local x = MinX - 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,2,1)
+                surface.SetTextPos(x + offsetx,MinY-LEFT_MARGIN+offsety)
+                surface.DrawText(str)
+                LEFT_MARGIN = LEFT_MARGIN - TEXT_MARGIN_INCREMENT
+            elseif rankRenderType == "Right" then
+                local x = MaxX + 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,0,1)
+                surface.SetTextPos(x + offsetx,MinY-RIGHT_MARGIN+offsety)
+                surface.DrawText(str)
+            elseif rankRenderType == "Bottom" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,0)
+                surface.SetTextPos(x + offsetx,MaxY+BOTTOM_MARGIN+offsety)
+                surface.DrawText(str)
+                BOTTOM_MARGIN = BOTTOM_MARGIN + TEXT_MARGIN_INCREMENT
+            end
+        end
+        if methamphetamine.mods["ESP"].enabled["Team"] then
+            if methamphetamine.mods["ESP"].colortype["Team"] == "Selected Color" then
+                SetDrawColor( colors["Team"] or color_white )
+            elseif methamphetamine.mods["ESP"].colortype["Team"] == "Team Color" then
+                SetDrawColor( teamcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Team"] == "Health Color" then
+                SetDrawColor( healthcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Team"] == "Rainbow" then
+                SetDrawColor( rainbow )
+            end
+            local teamRenderType = methamphetamine.mods["ESP"].types["Team"]
+            local str = methamphetamine.GetTeamName(v:Team())
+            if teamRenderType == "Top" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,4)
+                surface.SetTextPos(x + offsetx,MinY-TOP_MARGIN-offsety)
+                surface.DrawText(str)
+                TOP_MARGIN = TOP_MARGIN + TEXT_MARGIN_INCREMENT
+            elseif teamRenderType == "Left" then
+                local x = MinX - 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,2,1)
+                surface.SetTextPos(x + offsetx,MinY-LEFT_MARGIN+offsety)
+                surface.DrawText(str)
+                LEFT_MARGIN = LEFT_MARGIN - TEXT_MARGIN_INCREMENT
+            elseif teamRenderType == "Right" then
+                local x = MaxX + 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,0,1)
+                surface.SetTextPos(x + offsetx,MinY-RIGHT_MARGIN+offsety)
+                surface.DrawText(str)
+            elseif teamRenderType == "Bottom" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,0)
+                surface.SetTextPos(x + offsetx,MaxY+BOTTOM_MARGIN+offsety)
+                surface.DrawText(str)
+                BOTTOM_MARGIN = BOTTOM_MARGIN + TEXT_MARGIN_INCREMENT
+            end
+        end
+        if methamphetamine.mods["ESP"].enabled["Health [Txt]"] then
+            if methamphetamine.mods["ESP"].colortype["Health [Txt]"] == "Selected Color" then
+                SetTextColor( colors["Health [Txt]"] or color_white )
+            elseif methamphetamine.mods["ESP"].colortype["Health [Txt]"] == "Team Color" then
+                SetTextColor( teamcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Health [Txt]"] == "Health Color" then
+                SetTextColor( healthcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Health [Txt]"] == "Rainbow" then
+                SetTextColor( rainbow )
+            end
+            local healthRenderType = methamphetamine.mods["ESP"].types["Health [Txt]"]
+            local str ="HP: "..v:Health()
+            if healthRenderType == "Top" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,4)
+                surface.SetTextPos(x + offsetx,MinY-TOP_MARGIN-offsety)
+                surface.DrawText(str)
+                TOP_MARGIN = TOP_MARGIN + TEXT_MARGIN_INCREMENT
+            elseif healthRenderType == "Left" then
+                local x = MinX - 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,2,1)
+                surface.SetTextPos(x + offsetx,MinY-LEFT_MARGIN+offsety)
+                surface.DrawText(str)
+                LEFT_MARGIN = LEFT_MARGIN - TEXT_MARGIN_INCREMENT
+            elseif healthRenderType == "Right" then
+                local x = MaxX + 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,0,1)
+                surface.SetTextPos(x + offsetx,MinY-RIGHT_MARGIN+offsety)
+                surface.DrawText(str)
+            elseif healthRenderType == "Bottom" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,0)
+                surface.SetTextPos(x + offsetx,MaxY+BOTTOM_MARGIN+offsety)
+                surface.DrawText(str)
+                BOTTOM_MARGIN = BOTTOM_MARGIN + TEXT_MARGIN_INCREMENT
+            end
+        end
+        if methamphetamine.mods["ESP"].enabled["Weapon"] then
+            if methamphetamine.mods["ESP"].colortype["Weapon"] == "Selected Color" then
+                SetTextColor( colors["Weapon"] or color_white )
+            elseif methamphetamine.mods["ESP"].colortype["Weapon"] == "Team Color" then
+                SetTextColor( teamcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Weapon"] == "Health Color" then
+                SetTextColor( healthcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Weapon"] == "Rainbow" then
+                SetTextColor( rainbow )
+            end
+            local weaponRenderType = methamphetamine.mods["ESP"].types["Weapon"]
+            local str = IsValid(v:GetActiveWeapon()) and v:GetActiveWeapon():GetClass() or "NONE"
+            if weaponRenderType == "Top" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,4)
+                surface.SetTextPos(x + offsetx,MinY-TOP_MARGIN-offsety)
+                surface.DrawText(str)
+                TOP_MARGIN = TOP_MARGIN + TEXT_MARGIN_INCREMENT
+            elseif weaponRenderType == "Left" then
+                local x = MinX - 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,2,1)
+                surface.SetTextPos(x + offsetx,MinY-LEFT_MARGIN+offsety)
+                surface.DrawText(str)
+                LEFT_MARGIN = LEFT_MARGIN - TEXT_MARGIN_INCREMENT
+            elseif weaponRenderType == "Right" then
+                local x = MaxX + 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,0,1)
+                surface.SetTextPos(x + offsetx,MinY-RIGHT_MARGIN+offsety)
+                surface.DrawText(str)
+            elseif weaponRenderType == "Bottom" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,0)
+                surface.SetTextPos(x + offsetx,MaxY+BOTTOM_MARGIN+offsety)
+                surface.DrawText(str)
+                BOTTOM_MARGIN = BOTTOM_MARGIN + TEXT_MARGIN_INCREMENT
+            end
+        end
+        if methamphetamine.mods["ESP"].enabled["Distance"] then
+            if methamphetamine.mods["ESP"].colortype["Distance"] == "Selected Color" then
+                SetTextColor( colors["Distance"] or color_white )
+            elseif methamphetamine.mods["ESP"].colortype["Distance"] == "Team Color" then
+                SetTextColor( teamcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Distance"] == "Health Color" then
+                SetTextColor( healthcolor )
+            elseif methamphetamine.mods["ESP"].colortype["Distance"] == "Rainbow" then
+                SetTextColor( rainbow )
+            end
+            local distanceRenderType = methamphetamine.mods["ESP"].types["Distance"]
+            local str = "Distance: "..distance
+            if distanceRenderType == "Top" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,4)
+                surface.SetTextPos(x + offsetx,MinY-TOP_MARGIN-offsety)
+                surface.DrawText(str)
+                TOP_MARGIN = TOP_MARGIN + TEXT_MARGIN_INCREMENT
+            elseif distanceRenderType == "Left" then
+                local x = MinX - 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,2,1)
+                surface.SetTextPos(x + offsetx,MinY-LEFT_MARGIN+offsety)
+                surface.DrawText(str)
+                LEFT_MARGIN = LEFT_MARGIN - TEXT_MARGIN_INCREMENT
+            elseif distanceRenderType == "Right" then
+                local x = MaxX + 1
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,0,1)
+                surface.SetTextPos(x + offsetx,MinY-RIGHT_MARGIN+offsety)
+                surface.DrawText(str)
+            elseif distanceRenderType == "Bottom" then
+                local x = MinX - ((MinX-MaxX)/2)
+                local offsetx,offsety = calculateTextOffSet(str,methamphetamine.default.font,1,0)
+                surface.SetTextPos(x + offsetx,MaxY+BOTTOM_MARGIN+offsety)
+                surface.DrawText(str)
+                BOTTOM_MARGIN = BOTTOM_MARGIN + TEXT_MARGIN_INCREMENT
+            end
         end
 
         
@@ -389,7 +664,7 @@ addHook("PreDrawHalos","methamphetamine.halos",function()
 
     for k , v in ipairs( GetAllPlayers() ) do
         if LocalPlayer():GetPos():DistToSqr( v:GetPos() ) > methamphetamine.mods["ESP"].Range*methamphetamine.mods["ESP"].Range then goto skipdrawing end
-        local teamcolor = GetTeamColor( v:Team() )
+        local teamcolor = methamphetamine.GetTeamColor( v:Team() )
         local healthcolor = Color( 255 * (1 - (v:Health() / v:GetMaxHealth())) , 255 * (v:Health()/v:GetMaxHealth()), 0 )
         local rainbow = HSVToColor( (CurTime()*20)%360,1,1 )
         local c = color_white
